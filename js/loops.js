422927cfa4abd033fcf87b128a16134c4a823cbf
Michael Seth <michael.a.seth175@gmail.com>
2021-02-04 15:17:12 -0500

    made the number guessing game

--- js/loops.js
+++ js/loops.js
@@ -1,1 +1,19 @@
-console.log("connected");
+let randomNum = Math.floor((Math.random() * 100) + 1);
+let userNum;
+
+
+let guess = 0;
+while (guess < 5) {
+    userNum = prompt("Guess a number between 1 and 100");
+    guess++;
+    console.log(randomNum);
+
+    if (userNum < randomNum) {
+    alert(`You have guessed too low.`)
+} else if (userNum > randomNum) {
+    alert(`You have guessed too high.`)
+} else {
+    alert(`You have guessed correctly.`)
+    break;
+}
+}
